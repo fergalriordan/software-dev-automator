21b4f98b6cd9fb6100603e7e5e77b73fbb64f224
Fergal Riordan <fergalriordan333@gmail.com>
2025-04-04 18:37:49 +0100

    Updated port to use environment variable

--- backend/server.py
+++ backend/server.py
@@ -62,4 +62,5 @@ def download_file(filename):
     return send_from_directory(os.path.abspath(OUTPUT_DIR), filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
